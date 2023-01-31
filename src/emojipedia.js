ed0443363c02692325329b0776d67ade421d9db9
Eunji Park <eunjipark1984@gmail.com>
2023-01-31 18:09:46 -0500

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,78 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "📁",
+    name: "Quote 1",
+    meaning:"file saveall"
+  },
+  {
+    id: 2,
+    emoji: "🌞",
+    name: "Quote 2",
+    meaning:
+      "You can now play in the sun"
+  },
+  {
+    id: 3,
+    emoji: "⏱️",
+    name: "Quote 3",
+    meaning:
+      "Just hold on for a moment"
+  },
+  {
+    id: 4,
+    emoji: "🎤",
+    name: "Quote 4",
+    meaning:
+      "Is there a question? I think microphone is open"
+  }
+  ,
+  {
+    id: 5,
+    emoji: "💻",
+    name: "Quote 5",
+    meaning:
+      "Do you need to share your screen Muhammad"
+  },
+  {
+    id: 6,
+    emoji: "💻",
+    name: "Quote 6",
+    meaning:"I'm good, thank you"
+  },
+  {
+    id: 7,
+    emoji: "💻",
+    name: "Quote 7",
+    meaning:"Do things the easy way"
+  },
+  {
+    id: 8,
+    emoji: "💻",
+    name: "Quote 8",
+    meaning:"I will just give others a moment before we get started"
+  },
+  {
+    id: 9,
+    emoji: "💻",
+    name: "Quote 9",
+    meaning:"You are now in a position to start with the exercise"
+  },
+  {
+    id: 10,
+    emoji: "💻",
+    name: "Quote 10",
+    meaning:"You can name it \"goatblower\" and it will still work"
+  },
+  {
+    id: 11,
+    emoji: "💻",
+    name: "Quote 11",
+    meaning:"Meow"
+  }
+
+
+
+];
+
+export default emojipedia;
